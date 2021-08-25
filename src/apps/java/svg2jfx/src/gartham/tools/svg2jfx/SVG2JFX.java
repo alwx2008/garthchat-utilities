0840105d87b96f2ac7b1acf93787cdb120ea9901
Gartham <gartham@dusttoash.org>
2021-08-24 21:00:36 -0500

    Create SVG2JFX.java

---
+++ src/apps/java/svg2jfx/src/gartham/tools/svg2jfx/SVG2JFX.java
@@ -0,0 +1,13 @@
+package gartham.tools.svg2jfx;
+
+public class SVG2JFX {
+
+	public static void main(String[] args) {
+		if (args.length == 1) {
+			// TODO Parse SVG Path.
+		} else {
+			System.out.println("Invalid number of arguments. Please provide the SVG Path as a single argument.");
+		}
+	}
+
+}
