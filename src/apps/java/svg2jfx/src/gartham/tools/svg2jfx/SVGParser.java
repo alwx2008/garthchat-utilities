8244ddb712a61f6b9737e45a4aeba94c5c12f468
Gartham <gartham@dusttoash.org>
2021-08-26 13:57:46 -0500

    Update SVGParser.java

--- src/apps/java/svg2jfx/src/gartham/tools/svg2jfx/SVGParser.java
+++ src/apps/java/svg2jfx/src/gartham/tools/svg2jfx/SVGParser.java
@@ -1,12 +1,23 @@
 package gartham.tools.svg2jfx;
 
 import zeale.mouse.utils.BufferedParser;
+import zeale.mouse.utils.CharacterParser;
+import zeale.mouse.utils.Parser;
 
 public class SVGParser extends BufferedParser<PathElement> {
 
+	private final CharacterParser in;
+
+	public SVGParser(CharacterParser in) {
+		this.in = in;
+	}
+
+	public SVGParser(Parser<Character> in) {
+		this(CharacterParser.from(in));
+	}
+
 	@Override
 	protected PathElement read() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
