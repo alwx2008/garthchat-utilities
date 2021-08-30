7467275a7958e2fdeac655380f094902bfc713ea
Gartham <gartham@dusttoash.org>
2021-08-29 23:35:35 -0500

    Update SVG2JFX.java

--- src/apps/java/svg2jfx/src/gartham/tools/svg2jfx/SVG2JFX.java
+++ src/apps/java/svg2jfx/src/gartham/tools/svg2jfx/SVG2JFX.java
@@ -1,30 +1,17 @@
 package gartham.tools.svg2jfx;
 
-import org.alixia.javalibrary.streams.CharacterStream;
-import org.alixia.javalibrary.streams.PeekableCharacterStream;
+import zeale.mouse.utils.CharacterParser;
 
 public class SVG2JFX {
 
-	private static PeekableCharacterStream in;
-
-	private static void parseWhitespace() {
-		while (in.peek() != -1 && (Character.isWhitespace(in.peek()) || in.peek() == ','))
-			in.next();
-	}
-
 	public static void main(String[] args) {
 		if (args.length == 1) {
-			in = PeekableCharacterStream.from(CharacterStream.from(args[0]));// Set up the CharStream object.
-			while (in.peek() != -1) {
-
-			}
-		} else {
+			SVGParser p = new SVGParser(CharacterParser.from(args[0]));
+			PathElement pe;
+			while ((pe = p.next()) != null)
+				System.out.println(pe.toJavaFXStatement());
+		} else
 			System.out.println("Invalid number of arguments. Please provide the SVG Path as a single argument.");
-		}
-	}
-
-	private static String parsePathElement() {
-
 	}
 
 }
